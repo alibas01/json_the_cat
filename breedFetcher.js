9c971726e215fcf23f53c9cffdceec311a1d00e4
Ali Bas <alibas01@gmail.com>
2021-01-15 13:21:54 -0500

    2 test passed

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,29 @@
 const request = require('request');
-let arg = process.argv;
-let breed = arg[2];
-const URL = `http://api.thecatapi.com/v1/breeds/search?q=${breed}`;
 
-request(URL, (err, response, body) => {
-  if (err) {
-    console.log(' err', err);
-  }
-  const catObj = JSON.parse(body);
-  if (catObj.length === 0) {
-    console.log('No results found');
-  } else {
-    console.log(catObj);
-  }
-});
+
+const fetchBreedDescription = function(breedName, callback) {
+  // const callback = (error, desc) => {
+  //   if (error) {
+  //     console.log('Error fetch details:', error);
+  //   } else {
+  //     console.log(desc);
+  //   }
+  // })
+  const URL = `http://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  
+  request(URL, (err, response, body) => {
+    const catObj = JSON.parse(body);
+    if (err) {
+      console.log("API Error", err.message);
+      return;
+    }
+    if (catObj.length === 0) {
+      callback("No results found.", null);
+      return;
+    }
+        
+    callback(null, catObj[0].description);
+  });
+};
+
+module.exports = {fetchBreedDescription};
